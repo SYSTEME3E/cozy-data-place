f4cc24101f51c1be6bb04a45ad15ba7f9a991438
SYSTEME3E <communautemiprec@gmail.com>
2026-04-09 12:55:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // important pour Capacitor (chemins relatifs)
   server: {
     host: "::",
     port: 8080,
