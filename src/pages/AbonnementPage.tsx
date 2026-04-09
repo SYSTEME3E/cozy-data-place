09d53220cab2d96b221ffed64f73d0acde12bca8
SYSTEME3E <communautemiprec@gmail.com>
2026-04-09 05:40:01 +0100

    Update AbonnementPage.tsx

--- src/pages/AbonnementPage.tsx
+++ src/pages/AbonnementPage.tsx
@@ -13,7 +13,6 @@ import { useNavigate } from "react-router-dom";
 // DONNÉES DE COMPARAISON
 // ─────────────────────────────────────────────
 
-
 const FEATURES_COMPARE = [
   {
     categorie: "Finance personnelle",
